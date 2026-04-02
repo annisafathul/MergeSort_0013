feb93f87d3e271f7b072471783f70a7fbd3ba56c
annisafathul <annisafathul18@gmail.com>
2026-04-02 15:13:09 +0700

    add validation for max array size (<=20)

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -3,3 +3,20 @@ using namespace std;
 
 int arr[20], B[20];
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Maksimal panjang array adalah 20";
+        }
+    
