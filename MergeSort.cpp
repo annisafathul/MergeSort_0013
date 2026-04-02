da7dd0011d37d55c75774e6dc23743fa09ece713
annisafathul <annisafathul18@gmail.com>
2026-04-02 15:36:11 +0700

    add output

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -81,4 +81,18 @@ void mergeSort(int low, int high)
     }
 }
 
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
 
+int main()
+{
+    input();
+    mergeSort(0, n - 1);
+    output();
+}
