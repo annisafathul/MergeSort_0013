08850bddd2f1f652a75ede8fcb9ad839b30055aa
annisafathul <annisafathul18@gmail.com>
2026-04-02 15:17:32 +0700

    add loop to input array elements

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -19,4 +19,15 @@ void input()
         {
             cout << "Maksimal panjang array adalah 20";
         }
-    
+    }
+
+    cout << "\n---------------------------" << endl;
+    cout << "\nInputkan isi element array" << endl;
+    cout << "---------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke - " << i << " : ";
+        cin >> arr[i];
+    }
+}
