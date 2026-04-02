59e45d11a3113e07d04ad9162d45d267da191818
annisafathul <annisafathul18@gmail.com>
2026-04-02 15:26:55 +0700

    add recursive call

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -39,3 +39,10 @@ void mergeSort(int low, int high)
         return;
     }
     int mid = (low + high) / 2;
+
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
